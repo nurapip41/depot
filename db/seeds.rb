e8071136e4e33e904f49d5c5710271452fbc3706
nurapip <nurapip@41studio.com>
2017-07-04 19:20:34 +0700

    add scaffold product list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+Product.create!(title: 'lean ruby hard way',
+	description: %{<p>'Learn Ruby The Hard Way is the same highly popular way for total beginners to finally learn how to code but for the Ruby programming language.
+	 Learn Ruby The Hard Way takes you from absolute zero to able to read and write basic Ruby to then understand other books on Ruby. 
+	 No experience necessary to begin, and you can even try the book out for free to see if the method works for you.
+	  If youve always wanted to learn to code but have no idea where to begin, then this book is for you.</p>'},
+	image_url: 'rubyonrails.png',
+	price: 23.0)
